a25afe6315154bfb42d5e1fd7eeb7f7141f9017b
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-07 12:38:37 -0700

    size of data

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the size of data type
+ * Return: (0) success
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+print("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
+print("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
+print("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(c));
+print("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(d));
+print("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
+return (0);
+}
