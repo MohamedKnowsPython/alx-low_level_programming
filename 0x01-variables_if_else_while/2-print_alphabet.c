da29de588f6026cd03243fdaf13fa5c69830c8e3
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-10 23:14:14 -0700

    alphabets printing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+for (char letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+putchar('\n');
+return (0);
+}
