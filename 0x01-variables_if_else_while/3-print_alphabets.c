49de84649128377c851ce6300a7a0537c302278d
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-11 00:39:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n = 97;
+int m = 65;
+while (n <= 122)
+{
+	putchar(n);
+}
+while (m <= 98)
+{
+	putchar(m);
+}
+putchar('\n');
+return (0);
+}
