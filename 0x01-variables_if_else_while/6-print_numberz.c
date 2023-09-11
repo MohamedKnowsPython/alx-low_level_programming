d35542e594ab4cf4d8da87df480e9297ed90c056
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-11 02:03:22 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: Always success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+	putchar(n + 48);
+}
+putchar('\n')
+return (0);
+}
