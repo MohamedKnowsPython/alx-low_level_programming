0863048b056fa63c9c2e606a39bb1803772ce0e7
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-11 03:13:45 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+int n;
+int l;
+for (n = 48; n <= 57; n++)
+{
+	putchar(n);
+}
+for (l = 97; l <= 102; l++)
+{
+	putchar(l);
+}
+putchar('\n');
+return (0);
+}
