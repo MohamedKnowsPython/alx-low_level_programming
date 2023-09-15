70ac8e8291d70148b2f95de956069d67c48de442
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-15 12:04:17 -0700

    multiplication of two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * mul - multiples two integer
+ * @j: - the first integer
+ * @k: - the second interger
  * Return: Always 0
  */
-int mul(int a, int b)
+
+int mul(int j, int k)
 {
-	int answer = a * b;
+	int answer = j * k;
 	return (answer);
 }
