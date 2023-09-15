eddf3fa18aa60824f92c8e0c696c9b31ff52c73d
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-15 14:29:00 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square of size
+ * @s: size of the square
+ * Return: void
+ */
+void print_square(int s)
+{
+	int i, j;
+
+	if (s <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < s; i++)
+	{
+		for (j = 0; j < (s); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
