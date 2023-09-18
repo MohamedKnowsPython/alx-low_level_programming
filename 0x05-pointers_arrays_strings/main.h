8e0aa476ccdeeca33bc11af552d670c0c9650b53
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-09-18 08:32:01 -0700

    pointers readme and main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+int _putchar(char c);
+void rev_string(char *s);
+int _putchar(char c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
