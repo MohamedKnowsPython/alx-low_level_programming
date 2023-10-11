ebc8834788707eef738234463e30c11afab5aead
MohamedKnowsPython <mohaknowspy@gmail.com>
2023-10-11 07:55:37 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @f: pointer
+ * @name: string
+ * Return: zero
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
